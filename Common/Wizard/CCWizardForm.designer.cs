6c1ba6da3ee4405d1f680488464ed2a1c31cdd72
damonlin <damonalbarn1351@hotmail.com>
2014-09-14 20:33:19 +0800

    Update UI

--- Common/Wizard/CCWizardForm.designer.cs
+++ Common/Wizard/CCWizardForm.designer.cs
@@ -34,18 +34,18 @@
             this.m_finishButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_backButton
             // 
             this.m_backButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.m_backButton.Font = new System.Drawing.Font("PMingLiU", 12F);
-            this.m_backButton.Location = new System.Drawing.Point(1309, 3);
+            this.m_backButton.Location = new System.Drawing.Point(3, 79);
             this.m_backButton.Name = "m_backButton";
-            this.m_backButton.Size = new System.Drawing.Size(133, 32);
+            this.m_backButton.Size = new System.Drawing.Size(133, 70);
             this.m_backButton.TabIndex = 0;
             this.m_backButton.Text = "< &上一頁";
             this.m_backButton.UseVisualStyleBackColor = true;
@@ -55,9 +55,9 @@
             // 
             this.m_nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.m_nextButton.Font = new System.Drawing.Font("PMingLiU", 12F);
-            this.m_nextButton.Location = new System.Drawing.Point(1448, 3);
+            this.m_nextButton.Location = new System.Drawing.Point(3, 155);
             this.m_nextButton.Name = "m_nextButton";
-            this.m_nextButton.Size = new System.Drawing.Size(133, 32);
+            this.m_nextButton.Size = new System.Drawing.Size(133, 70);
             this.m_nextButton.TabIndex = 1;
             this.m_nextButton.Text = "&下一頁 >";
             this.m_nextButton.UseVisualStyleBackColor = true;
@@ -67,9 +67,9 @@
             // 
             this.m_cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.m_cancelButton.Font = new System.Drawing.Font("PMingLiU", 12F);
-            this.m_cancelButton.Location = new System.Drawing.Point(1170, 3);
+            this.m_cancelButton.Location = new System.Drawing.Point(3, 231);
             this.m_cancelButton.Name = "m_cancelButton";
-            this.m_cancelButton.Size = new System.Drawing.Size(133, 32);
+            this.m_cancelButton.Size = new System.Drawing.Size(133, 70);
             this.m_cancelButton.TabIndex = 2;
             this.m_cancelButton.Text = "取消";
             this.m_cancelButton.UseVisualStyleBackColor = true;
@@ -80,9 +80,9 @@
             // 
             this.m_finishButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.m_finishButton.Font = new System.Drawing.Font("PMingLiU", 12F);
-            this.m_finishButton.Location = new System.Drawing.Point(1587, 3);
+            this.m_finishButton.Location = new System.Drawing.Point(3, 3);
             this.m_finishButton.Name = "m_finishButton";
-            this.m_finishButton.Size = new System.Drawing.Size(133, 32);
+            this.m_finishButton.Size = new System.Drawing.Size(133, 70);
             this.m_finishButton.TabIndex = 3;
             this.m_finishButton.Text = "&列印";
             this.m_finishButton.UseVisualStyleBackColor = true;
@@ -93,36 +93,36 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1723, 908);
+            this.panel1.Size = new System.Drawing.Size(1469, 958);
             this.panel1.TabIndex = 4;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 254F));
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.81328F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.186722F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1729, 964);
             this.tableLayoutPanel1.TabIndex = 5;
             // 
-            // flowLayoutPanel1
+            // flowLayoutPanel2
             // 
-            this.flowLayoutPanel1.Controls.Add(this.m_finishButton);
-            this.flowLayoutPanel1.Controls.Add(this.m_nextButton);
-            this.flowLayoutPanel1.Controls.Add(this.m_backButton);
-            this.flowLayoutPanel1.Controls.Add(this.m_cancelButton);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 917);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1723, 44);
-            this.flowLayoutPanel1.TabIndex = 5;
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.flowLayoutPanel2.Controls.Add(this.m_finishButton);
+            this.flowLayoutPanel2.Controls.Add(this.m_backButton);
+            this.flowLayoutPanel2.Controls.Add(this.m_nextButton);
+            this.flowLayoutPanel2.Controls.Add(this.m_cancelButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(1478, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(248, 958);
+            this.flowLayoutPanel2.TabIndex = 6;
             // 
             // CCWizardForm
             // 
@@ -133,7 +133,7 @@
             this.Name = "CCWizardForm";
             this.Size = new System.Drawing.Size(1729, 964);
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -146,6 +146,6 @@
         protected System.Windows.Forms.Button m_finishButton;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
